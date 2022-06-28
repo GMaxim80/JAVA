1b3ef30f91e3f1f724f438e79e7a0ea9fe3fcc4a
GMaxim80 <mgubanov80@gmail.com>
2022-06-28 23:26:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double d1 = 20; // равно 1 мили
+        double d2 = 2000; // цена билета
+
+        System.out.println("Сколько миль будет начислено");
+        System.out.println((d2 / d1) + "миль начислено");
+    }
+}
