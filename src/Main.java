56c8c263a0215eeb4c6768fff9bb38d1db269743
GMaxim80 <mgubanov80@gmail.com>
2022-07-09 22:40:33 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        double d1 = 20; // равно 1 мили
-        double d2 = 2000; // цена билета
+        int price = 2000;
+        int miles = price / 20;
 
         System.out.println("Сколько миль будет начислено");
-        System.out.println((d2 / d1) + "миль начислено");
+        System.out.println(miles + "миль");
     }
 }
